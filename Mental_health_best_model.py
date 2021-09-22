ef8e48e95f3bc4a1113eb7bf4e246a6a2c5b9f37
Amr Elbana <amr32009363@gmail.com>
2021-09-22 17:06:08 +0200

    Add files via upload

---
+++ Mental_health_best_model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=1)
+
+# Average CV score on the training set was: 0.7889718774107581
+exported_pipeline = BernoulliNB(alpha=0.1, fit_prior=True)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 1)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
